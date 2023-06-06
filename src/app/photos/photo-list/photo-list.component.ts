3449508768b7e89bfd9bd7f55f40ae09c79cd1cf
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-06-06 10:19:13 -0300

    Added loadMore button function

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { debounceTime } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 
 import { Photo } from 'src/utils/interfaces/photo.Interface';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -15,8 +16,14 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
   
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) {}
   
   onKeyUp(target: EventTarget | null) {
     if (target instanceof HTMLInputElement) {
@@ -25,6 +32,7 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit():void {
+    this.userName = this.activatedRoute.snapshot.params['userName'];
     this.photos = this.activatedRoute.snapshot.data['photos'];
     this.debounce
       .pipe(debounceTime(300))
@@ -33,6 +41,15 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if(!photos.length) this.hasMore = false;
+      })
   }
 
 }
