e6c51adef7ab1938287fa576620d3be4600c8af3
Ranayke Boni Ferreira <ranayke.ferreira@wedotec.com.br>
2023-07-07 14:33:08 -0300

    Added progress indicator when upload new photo

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -4,6 +4,8 @@ import { PhotoService } from '../photo/photo.service';
 import { Router } from '@angular/router';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { UserService } from 'src/app/core/user/user.service';
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'ap-photo-form',
@@ -15,6 +17,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm!: FormGroup;
   file!: File;
   preview!: string;
+  percentDone = 0;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -37,9 +40,19 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments')?.value;
     this.photoService
       .upload(description, allowComments, this.file)
-      .subscribe(() => {        
-        this.alertService.success('Upload completed', true)
+      .pipe(finalize(() => {
         this.router.navigate(['/user', this.userService.getUserName()])
+      }))
+      .subscribe((event: HttpEvent<any>) => {
+        if(event.type == HttpEventType.UploadProgress) {
+          this.percentDone = Math.round(100 * event.loaded / event.total!)
+        } else if(event instanceof HttpResponse) {
+          this.alertService.success('Upload completed', true)
+        }
+      },
+      err => {
+        console.log(err);
+        this.alertService.danger('Upload error', true);
       })
   }
 
